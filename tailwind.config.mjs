15b80aec94f6eaf15d09ce72110a492f7acb8c41
Ryan Trainor <rptrainor@gmail.com>
2024-01-22 11:02:08 -0300

    add brand colors to tailwind congig

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brand_gray: "#A7B5CC",
+        brand_aqua: "#5DA5FA",
+        brand_orange: "#F4C47C",
+        brand_cyan: "#3CD8E2",
+        brand_pink: "#FF1B8D",
+        brand_yellow: "#FFDA00",
+        brand_blue: "#1BB3FF",
+        brand_black: "#181E23",
+      },
+    },
+  },
+  plugins: [],
+};
