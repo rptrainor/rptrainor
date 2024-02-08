567cf5061e99bc15a64d0f00fdfd9a0a58a2fa5e
Ryan Trainor <rptrainor@gmail.com>
2024-02-08 16:35:02 -0300

    testing some improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,23 @@
-import { defineConfig, passthroughImageService } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig, passthroughImageService } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rptrainor.com",
-  output: "server",
-  prefetch: true,
-  image: {
-    service: passthroughImageService(),
-  },
-  adapter: cloudflare({
-    imageService: "passthrough",
-  }),
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap()],
-});
+	site: 'https://rptrainor.com',
+	output: 'server',
+	prefetch: true,
+	image: {
+		service: passthroughImageService()
+	},
+	adapter: cloudflare({
+		imageService: 'passthrough'
+	}),
+	integrations: [
+		tailwind({
+			applyBaseStyles: false
+		}),
+		sitemap()
+	]
+})
