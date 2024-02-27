04be55eed53849778b8fc670ca2822cee3cdc0fa
Ryan Trainor <rptrainor@gmail.com>
2024-02-27 14:47:08 -0300

    add handrawn font to pubic

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+				Handrawn: ['Handrawn', 'Sans', ...defaultTheme.fontFamily.sans]
+			},
       colors: {
         brand_pink: "#FF1B8D",
         brand_yellow: "#FFDA00",
         brand_blue: "#1BB3FF",
         brand_black: "#181E23",
-      },
+      }
     },
   },
   plugins: [],
