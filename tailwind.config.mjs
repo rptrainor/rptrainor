5059f24c2d54bb85da8e8c6f214a374c260b10ef
Ryan Trainor <rptrainor@gmail.com>
2024-02-28 11:49:10 -0300

    handrawn layout changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-				Handrawn: ['Handrawn', 'Sans', ...defaultTheme.fontFamily.sans]
-			},
       colors: {
         brand_pink: "#FF1B8D",
         brand_yellow: "#FFDA00",
