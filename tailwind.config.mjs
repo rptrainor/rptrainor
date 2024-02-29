9619362ee7f3d64511522733e346a55021ddf77a
Ryan Trainor <rptrainor@gmail.com>
2024-02-29 20:51:20 -0300

    use tailwind for the fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
+        handrawn: ['Delicious Handrawn', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         brand_pink: "#FF1B8D",
         brand_yellow: "#FFDA00",
