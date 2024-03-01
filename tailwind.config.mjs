09c40e12cae3f3edb8f460c52382b0c372c168dd
Ryan Trainor <rptrainor@gmail.com>
2024-02-29 21:07:40 -0300

    remove fonts from tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
-        handrawn: ['Delicious Handrawn', ...defaultTheme.fontFamily.sans],
-      },
       colors: {
         brand_pink: "#FF1B8D",
         brand_yellow: "#FFDA00",
