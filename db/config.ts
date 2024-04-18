6e1cdc9b4d994c24d5cf887b0218fde404c550cd
Ryan Trainor <rptrainor@gmail.com>
2024-04-18 10:34:16 -0300

    add db files

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,14 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable, NOW } from 'astro:db';
 
+const Query = defineTable({
+  columns: {
+    web_page_url: column.text(),
+    searchedAt: column.date({ default: NOW, optional: true }),
+  }
+})
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    Query
+  }
 });
