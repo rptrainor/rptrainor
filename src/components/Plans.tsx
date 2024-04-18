0dacfa0c7b0bcceed6cfab3e6942c3a4907761da
Ryan Trainor <rptrainor@gmail.com>
2024-04-18 18:27:26 -0300

    update payment links

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -14,14 +14,14 @@ const Plans = () => {
             </h3>
 
             <p class="flex items-baseline gap-x-1">
-              <span class="text-4xl font-bold tracking-tight text-white">$834</span>
+              <span class="text-4xl font-bold tracking-tight text-white">$1,247</span>
               <span class="text-sm font-semibold leading-6 text-gray-300">/month</span>
             </p>
             <small class="block text-sm leading-snug text-gray-300 md:h-24 sm:h-28 xs:h-20 h-32 grow lg:h-20">
             Our comprehensive Done-For-You service includes expertly crafted campaigns and continuous management, designed to stabilize your lead generation and ensure sustained business growth. Experience the peace of mind that comes from having a steady stream of high-quality leads.
             </small>
             <a
-              href="https://buy.stripe.com/aEU9AK2Ko2HicLufZ0"
+              href="https://buy.stripe.com/28ofZ884Ia9KaDmcMQ"
               aria-describedby="tier-startup"
               class="block rounded-md bg-white px-3 py-2 text-center text-lg font-semibold leading-6 text-background shadow-sm hover:animate-hover-pop focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
@@ -35,14 +35,14 @@ const Plans = () => {
             <h3 id="tier-startup" class="text-xl font-bold leading-8 tracking-tight text-white sm:h-16 md:h-8 xs:h-8 h-16">
               Build Only Service</h3>
             <p class="flex items-baseline gap-x-1">
-              <span class="text-4xl font-bold tracking-tight text-white">$457</span>
+              <span class="text-4xl font-bold tracking-tight text-white">$873</span>
               <span class="text-sm font-semibold leading-6 text-gray-300">once</span>
             </p>
             <small class="block text-sm leading-snug text-gray-300 md:h-24 sm:h-28 xs:h-20 h-32 grow lg:h-20">
             Invest once and reap the benefits indefinitely with a high-quality, professionally designed landing page crafted to convert visitors into leads efficiently.
             </small>
             <a
-              href="https://buy.stripe.com/6oE28i1Gk3Lm3aUdQT"
+              href="https://buy.stripe.com/fZedR00Cgeq06n6149"
               aria-describedby="tier-startup"
               class="block rounded-md bg-white px-3 py-2 text-center text-lg font-semibold leading-6 text-background shadow-sm hover:animate-hover-pop focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
