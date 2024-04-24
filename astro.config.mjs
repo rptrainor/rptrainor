18123facae2452bbf30d49cd63b2b2fa964e00ac
Ryan Trainor <rptrainor@gmail.com>
2024-04-24 20:27:16 -0300

    remove posthog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,20 @@
-import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
-import db from "@astrojs/db";
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
-import sitemap from "@astrojs/sitemap";
-
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
+import db from '@astrojs/db'
+import tailwind from '@astrojs/tailwind'
+import solidJs from '@astrojs/solid-js'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rptrainor.com',
-  output: 'server',
-  prefetch: true,
-  adapter: cloudflare({
-    imageService: 'passthrough',
-    platformProxy: {
-      enabled: true
-    }
-  }),
-  integrations: [tailwind(), sitemap(), db(), solidJs(), partytown({
-    config: {
-      forward: ['posthog'],
-    }
-  })]
-});
+	site: 'https://rptrainor.com',
+	output: 'server',
+	prefetch: true,
+	adapter: cloudflare({
+		imageService: 'passthrough',
+		platformProxy: {
+			enabled: true
+		}
+	}),
+	integrations: [tailwind(), sitemap(), db(), solidJs()]
+})
