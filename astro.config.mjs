d778306db77065e983e4d98c979f83e6ef1da35a
Ryan Trainor <rptrainor@gmail.com>
2024-04-24 15:52:01 -0300

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rptrainor.com',
-	output: 'server',
-	prefetch: true,
-	adapter: cloudflare({
-		imageService: 'passthrough',
-		platformProxy: {
+  site: 'https://rptrainor.com',
+  output: 'server',
+  prefetch: true,
+  adapter: cloudflare({
+    imageService: 'passthrough',
+    platformProxy: {
       enabled: true
     }
-	}),
-	integrations: [
-		tailwind(),
-		sitemap(),
-		db(),
-		solidJs()
-	]
-})
+  }),
+  integrations: [tailwind(), sitemap(), db(), solidJs(), partytown({
+    config: {
+      forward: ['dataLayer.push', 'posthog'],
+    }
+  })]
+});
