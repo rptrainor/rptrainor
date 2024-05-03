a2569e649aecdd11348e99adeacd46b15f733603
Ryan Trainor <rptrainor@gmail.com>
2024-05-03 00:21:04 -0300

    add partytown with bento

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
-import { defineConfig } from 'astro/config'
-import cloudflare from '@astrojs/cloudflare'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rptrainor.com',
-	output: 'server',
-	prefetch: true,
-	adapter: cloudflare({
-		imageService: 'passthrough',
-		platformProxy: {
-			enabled: true
-		}
-	}),
-	integrations: [tailwind(), sitemap()]
-})
+  site: 'https://rptrainor.com',
+  output: 'server',
+  prefetch: true,
+  adapter: cloudflare({
+    imageService: 'passthrough',
+    platformProxy: {
+      enabled: true
+    }
+  }),
+  integrations: [tailwind(), sitemap(), partytown({
+    config: {
+      forward: ['addEventListener'],
+    },
+  })]
+});
