806c270a486e223e54b6df2fbf33f62d2d7fd015
Ryan Trainor <rptrainor@gmail.com>
2024-05-17 01:18:54 -0300

    draft hero section

--- src/constants/PostsSummary.ts
+++ src/constants/PostsSummary.ts
@@ -2,47 +2,57 @@ export type PostsSummaryType = {
   title: string
   description: string | undefined
   date: string
+  slug: string
 };
 
 export const PostsSummary = [
   {
     title: 'Improve Shaan Puri\'s Blog',
     description: "How to Rebuild Shaan Puri's Blog with Laravel, Cloudflare, Nuxt, Vue, and Tailwind, so that the Home Page Loads in 1/3rd of the Time",
-    date: '2024-05-16'
+    date: '2024-05-16',
+    slug: 'improve-shaan-puri-blog'
+
   },
   {
-    title: 'Deploy Laravel Server',
+    title: 'Deploy a Laravel Server',
     description: 'How to Deploy a Laravel Web Server',
-    date: '2024-05-15'
+    date: '2024-05-15',
+    slug: 'deploy-a-laravel-server'
   },
   {
     title: 'Deploy Nuxt on Cloudflare Pages',
     description: 'How to Create a Nuxt App on Cloudflare Pages',
-    date: '2024-05-14'
+    date: '2024-05-14',
+    slug: 'deploy-nuxt-on-cloudflare-pages'
   },
   {
-    title: 'Speed up Beehiiv form page load time',
+    title: 'Speed up Beehiiv form page load',
     description: "How to Reduce Your Page Load Time While Still Collecting Beehiiv Subscribers",
-    date: '2024-05-13'
+    date: '2024-05-13',
+    slug: 'speed-up-beehiiv-form-page-load'
   },
   {
     title: 'Perfect Core Web Vitals Scores',
     description: "How to Get Perfect Core Web Vitals Scores",
-    date: '2024-05-12'
+    date: '2024-05-12',
+    slug: 'perfect-core-web-vitals-scores'
   },
   {
     title: 'Deploy Cloudflare Workers',
     description: 'How to Deploy a Cloudflare Worker',
-    date: '2024-05-11'
+    date: '2024-05-11',
+    slug: 'deploy-cloudflare-workers'
   },
   {
     title: 'Faster Pages with Cloudflare Workers',
     description: 'How to Use Cloudflare Workers to Reduce Latency Between Your Client and Backend',
-    date: '2024-05-10'
+    date: '2024-05-10',
+    slug: 'faster-pages-with-cloudflare-workers'
   },
   {
     title: "Golden Ratio with TailwindCSS",
     description: "How to Create Golden Ratio Layouts with TailwindCSS",
-    date: '2024-05-09'
+    date: '2024-05-09',
+    slug: 'golden-ratio-with-tailwindcss'
   }
 ]
