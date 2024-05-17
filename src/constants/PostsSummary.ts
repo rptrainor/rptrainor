f37bda96f2badf6702517d94ba0ba1d50ad0598d
Ryan Trainor <rptrainor@gmail.com>
2024-05-17 01:04:24 -0300

    add post summaries

---
+++ src/constants/PostsSummary.ts
@@ -0,0 +1,48 @@
+export type PostsSummaryType = {
+  title: string
+  description: string | undefined
+  date: string
+};
+
+export const PostsSummary = [
+  {
+    title: 'Improve Shaan Puri\'s Blog',
+    description: "How to Rebuild Shaan Puri's Blog with Laravel, Cloudflare, Nuxt, Vue, and Tailwind, so that the Home Page Loads in 1/3rd of the Time",
+    date: '2024-05-16'
+  },
+  {
+    title: 'Deploy Laravel Server',
+    description: 'How to Deploy a Laravel Web Server',
+    date: '2024-05-15'
+  },
+  {
+    title: 'Deploy Nuxt on Cloudflare Pages',
+    description: 'How to Create a Nuxt App on Cloudflare Pages',
+    date: '2024-05-14'
+  },
+  {
+    title: 'Speed up Beehiiv form page load time',
+    description: "How to Reduce Your Page Load Time While Still Collecting Beehiiv Subscribers",
+    date: '2024-05-13'
+  },
+  {
+    title: 'Perfect Core Web Vitals Scores',
+    description: "How to Get Perfect Core Web Vitals Scores",
+    date: '2024-05-12'
+  },
+  {
+    title: 'Deploy Cloudflare Workers',
+    description: 'How to Deploy a Cloudflare Worker',
+    date: '2024-05-11'
+  },
+  {
+    title: 'Faster Pages with Cloudflare Workers',
+    description: 'How to Use Cloudflare Workers to Reduce Latency Between Your Client and Backend',
+    date: '2024-05-10'
+  },
+  {
+    title: "Golden Ratio with TailwindCSS",
+    description: "How to Create Golden Ratio Layouts with TailwindCSS",
+    date: '2024-05-09'
+  }
+]
