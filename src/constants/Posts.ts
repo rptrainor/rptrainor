d65c44d4102bc4650565d0094ee7242d723fe5b0
Ryan Trainor <rptrainor@gmail.com>
2024-05-17 23:55:14 -0300

    ediiting blog posts

--- src/constants/Posts.ts
+++ src/constants/Posts.ts
@@ -42,76 +42,77 @@ export const Posts = [
     steps: [
       {
         type: 'header',
-        text: "Cloudflare Pages and using the least amount of Javascript possible is your path to success",
+        text: "Hosting on Cloudflare Pages and using as little Javascript as possible is your path to success",
       },
       {
         type: 'paragraph',
-        text: "First create a new web app with your your framework of choice. I used Nuxt for this one, but I have also been using Astro with Solid, because I like the island architecture"
+        text: "first, create a new web app with your framework of choice. I used Nuxt for this one, but Astro and Solid is a good combo too with the island architecture"
       },
       {
         type: 'code',
-        text: "pnpm create cloudflare@latest my-nuxt-app -- --framework=nuxt",
+        text: "pnpm create cloudflare@latest NAME_OF_YOUR_APP_HERE",
       },
       {
         type: 'paragraph',
-        text: "Download your font locally, you want to minimize the the requests that your app makes to external services"
+        text: "download your font locally. You want to minimize the requests that your app makes to external services"
       },
       {
         type: 'paragraph',
-        text: "TailwindCSS is your best choice for CSS. You get CSS that is already highly optimized, and it is going to have a smaller bundle size that any other framework or your hand written CSS classes"
+        text: "TailwindCSS is your best choice for CSS. You get CSS that is already highly optimized, and it is going to have a smaller bundle size than any other framework or your hand written CSS classes"
       },
       {
         type: 'paragraph',
-        text: "Look for oppertunities to offload tasks to the server. For example, I replaced the Beehiiv newsletter subscription forms that were slow loading iframes with HTML, CSS, and a POST request to our server"
+        text: "look for opportunities to offload tasks to the server. For example, I replaced the Beehiiv newsletter subscription forms that were slow loading iframes with HTML, CSS, and a POST request to our server"
       },
       {
         type: 'paragraph',
-        text: "Run your page through Google's Pagespeed Insights test, and look for opportunities to improve. For example, I found that I could optimize my images, and that I could offload some of the third party scripts to Cloudflare Workers"
+        text: "run your page through Google's Pagespeed Insights test, and look for areas that you can improve"
       },
       {
         type: 'paragraph',
-        text: "Convert your images to WebP format, and serve the correct image dimensions for different screen sizes"
+        text: "convert your images to WebP format, and serve the correct image dimensions for different screen sizes"
       },
       {
         type: 'paragraph',
-        text: "To avoid layout shifts, set HTML elements to have fixed heights and widths. Also, think about server side rendering or what is the most graceful way you can hanlde the loading to rendering state"
+        text: "to avoid layout shifts, set HTML elements to have fixed heights and widths. Also, think about server side rendering or what is the most graceful way you can handle any loading state"
       },
       {
         type: 'header',
-        text: "those are the tips that I have for building a fast loading web app."
+        text: "And those are the tips that I have for building a fast loading web app"
       },
       {
-        type: 'paragraph',
-        text: "Get the data as close as possible to the client, and use the least amount of Javascript and network requests as possible. This is the way to build a fast loading web app."
+        type: 'header',
+        text: "Get the data as close as possible to the client. Use the least amount of Javascript and network requests as possible. This is the way to build a fast loading web app"
       },
       {
         type: 'link',
-        text: "See for yourself: https://shaan-nuxt.pages.dev",
+        text: "see a live example of the rebuilt app here:",
         url: 'https://shaan-nuxt.pages.dev',
+      },
+      {
+        type: 'link',
+        text: "check out the code for the clientside app here:",
+        url: 'https://github.com/rptrainor/shaan-nuxt',
       }
     ]
   },
   {
-    title: 'Deploy a Laravel Server',
+    title: 'Deploy a RESTful Laravel Server',
     description: 'How to Deploy a Laravel Web Server',
     date: '2024-05-15',
     slug: 'deploy-a-laravel-server',
     steps: [
       {
-        type: 'paragraph',
-        text: 'First you need to have PHP and Composer are installed on your local machine. You can check if you have PHP installed by running `php -v` and you can check if you have Composer installed by running `composer -v`'
+        type: 'header',
+        text: "Let's start by creating a basic Laravel app",
       },
       {
-        type: 'header',
-        text: "Create a basic Laravel app",
+        type: 'paragraph',
+        text: 'first you need to have PHP and Composer are installed on your local machine. Then'
       },
       {
         type: 'code',
-        text: "composer create-project laravel/laravel library-api && cd library-api",
-      },
-      {
-        type: 'header',
-        text: 'Make sure you are the root of your Laravel app and run the app to make sure it worked',
+        text: "composer create-project laravel/laravel laravel-api && cd laravel-api",
       },
       {
         type: 'code',
@@ -123,11 +124,11 @@ export const Posts = [
       },
       {
         type: 'paragraph',
-        text: "Start with thinking about what data you want to store in your database with this API and what opperations you want to perform on that data"
+        text: "start with thinking about what data you want to store in your database with this API and what opperations you want to perform on that data"
       },
       {
         type: 'paragraph',
-        text: "Now that you know what data you want to store, you can create models and a schema to represent that data. I used the plural here, but I reccomend starting with a single model and table, and then adding more as you need them"
+        text: "now that you know what data you want to store, you can create models and a schema to represent that data. I used the plural here, but I reccomend starting with a single model and table, and then adding more as you need them"
       },
       {
         type: 'code',
@@ -135,7 +136,7 @@ export const Posts = [
       },
       {
         type: 'paragraph',
-        text: 'This will create a new model in your `app/models` file, and a new migration file in your `database/migrations` file. In your new migration file, update the up function, with the properties of your new table that you defined in your schema'
+        text: 'this will create a new model in your `app/models` file, and a new migration file in your `database/migrations` file. In your new migration file, update the up function, with the properties of your new table that you defined in your schema'
       },
       {
         type: 'header',
@@ -155,11 +156,11 @@ export const Posts = [
       },
       {
         type: 'paragraph',
-        text: 'Now you have a new controller file in `app\Http\Controllers\`. You can update and add methods to this controller to define the business logic for your API routes'
+        text: 'now you have a new controller file in `app\Http\Controllers\`. You can update and add methods to this controller to define the business logic for your API routes'
       },
       {
         type: 'paragraph',
-        text: "Think about CRUD: create, read, update, and delete. Think about what opperations that you want to perform on the data you are storing in your database. And then define the methods in your controller that will perform those opperations"
+        text: "think about CRUD: create, read, update, and delete. Think about what opperations that you want to perform on the data you are storing in your database. And then define the methods in your controller that will perform those opperations"
       },
       {
         type: 'header',
@@ -167,7 +168,7 @@ export const Posts = [
       },
       {
         type: 'paragraph',
-        text: 'In `routes/api.php` you can define the routes for your API. You can use the `Route::get`, `Route::post`, `Route::put`, and `Route::delete` methods to define the routes for your API. These methods generally match with CRUD actions or Create, Read, Update, and Delete'
+        text: 'in `routes/api.php` you can define the routes for your API. You can use the `Route::get`, `Route::post`, `Route::put`, and `Route::delete` methods to define the routes for your API. These methods generally match with CRUD actions or Create, Read, Update, and Delete'
       },
       {
         type: 'header',
@@ -175,7 +176,7 @@ export const Posts = [
       },
       {
         type: 'paragraph',
-        text: "You can test your new Laravel API by running `php artisan serve` and then making requests to the routes you defined in `routes/api.php`. You can use a tool like Postman to make requests to your API"
+        text: "you can test your new Laravel API by running `php artisan serve` and then making requests to the routes you defined in `routes/api.php`. You can use a tool like Postman to make requests to your API"
       },
       {
         type: 'header',
@@ -184,6 +185,11 @@ export const Posts = [
       {
         type: 'paragraph',
         text: "There are a variety of ways to deploy your Laravel web server. Laravel Vapor is a serverless option that is supported by the Laravel team. Laravel Forge is a serverful option that is also supported by the Laravel team. You can also deploy your Laravel app on services like Digital Ocean, Fly.io, or shared storage."
+      },
+      {
+        type: 'link',
+        text: "check out the code for the Laravel RESTful API server here:",
+        url: 'https://github.com/rptrainor/shaan-api',
       }
     ]
   },
@@ -264,6 +270,21 @@ export const Posts = [
       {
         type: 'paragraph',
         text: "Enjoy your new Nuxt app on Cloudflare Pages. You now have a web app that is fast, secure, and scalable"
+      },
+      {
+        type: 'link',
+        text: "see a live example of the rebuilt app here:",
+        url: 'https://shaan-nuxt.pages.dev',
+      },
+      {
+        type: 'link',
+        text: "check out the code for the clientside app here:",
+        url: 'https://github.com/rptrainor/shaan-nuxt',
+      },
+      {
+        type: 'link',
+        text: "you can see the Cloudflare Worker that we use as a cache layer here:",
+        url: 'https://github.com/rptrainor/shaan-cloudflare-worker',
       }
     ]
   }
